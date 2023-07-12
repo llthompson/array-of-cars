7f88b94cef53acc3e936b4d5bf8bfaa2e67e4fb4
llthompson <lisalacythompson+github@gmail.com>
2023-07-11 19:28:02 -0500

    final run

--- main.js
+++ main.js
@@ -61,3 +61,16 @@ console.log('After Shift:', pets);
 pets.unshift('turtle')
 
 console.log('Unshift and a Turtle:', pets);
+
+
+const numbers = [23, 45, 0, 2, 8, 44, 100, 1, 3, 91, 34]
+
+function addTwo(num, index, arr) {
+    return num + 2;
+}
+
+numbers.forEach((num, index, arr) => {
+    arr[index] = addTwo(num, index, arr);
+});
+
+console.log('Added Two:', numbers);
